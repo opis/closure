955e280de1e909627c80bc2d89aabfbc2b5bf5dc
msarca <marius.sarca@gmail.com>
2014-10-18 10:23:05 +0300

    Added autoload file

---
+++ autoload.php
@@ -0,0 +1,32 @@
+<?php
+
+spl_autoload_register(function($class){
+   
+    $class = ltrim($class, '\\');
+    $dir = __DIR__ . '/lib';
+    $namespace = 'Opis\Closure';
+    
+    if(strpos($class, $namespace) === 0)
+    {
+        $class = substr($class, strlen($namespace));
+        $path = '';
+        if(($pos = strripos($class, '\\')) !== FALSE)
+        {
+            $path = str_replace('\\', '/', substr($class, 0, $pos)) . '/';
+            $class = substr($class, $pos + 1);
+        }
+        $path .= str_replace('_', '/', $class) . '.php';
+        $dir .= '/' . $path;
+        
+        if(file_exists($dir))
+        {
+            include $dir;
+            return true;
+        }
+        
+        return false;
+    }
+    
+    return false;
+
+});
