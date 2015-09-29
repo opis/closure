18577ecc4669db9cf31600544bff86e91774eced
msarca <marius.sarca@gmail.com>
2015-09-29 23:17:33 +0300

    A bugfix and other improvements

--- lib/ReflectionClosure.php
+++ lib/ReflectionClosure.php
@@ -11,9 +11,8 @@
 namespace Opis\Closure;
 
 use Closure;
-use ReflectionFunction;
 use SplFileObject;
-
+use ReflectionFunction;
 
 class ReflectionClosure extends ReflectionFunction
 {
@@ -21,9 +20,11 @@ class ReflectionClosure extends ReflectionFunction
     protected $tokens;
     protected $hashedName;
     protected $useVariables;
+    protected $isStaticClosure;
     
     protected static $files = array();
     protected static $classes = array();
+    protected static $structures = array();
     
     public function __construct(Closure $closure, $code = null)
     {
@@ -104,12 +105,18 @@ class ReflectionClosure extends ReflectionFunction
         if(!isset(static::$classes[$key]))
         {
             $classes = array();
+            $structures = array();
             $tokens = &$this->getFileTokens();
             
             $open = 0;
             $state = 'start';
             $class = '';
             $alias = '';
+            
+            $startLine = $endLine = 0;
+            $structType = $structName = '';
+            
+            $hasTraitSuppot = defined('T_TRAIT');
             
             foreach($tokens as &$token)
             {
@@ -124,16 +131,20 @@ class ReflectionClosure extends ReflectionFunction
                             {
                                 case T_CLASS:
                                 case T_INTERFACE:
-                                    $state = 'structure';
+                                    $state = 'before_structure';
+                                    $startLine = $token[2];
+                                    $structType = $token[0] == T_CLASS ? 'class' : 'interface';
                                     break;
                                 case T_USE:
                                     $state = 'use';
                                     $class = $alias = '';
                                     break;
                                 default:
-                                    if(defined('T_TRAIT') && $token[0] == T_TRAIT)
+                                    if($hasTraitSuppot && $token[0] == T_TRAIT)
                                     {
-                                        $state = 'structure';
+                                        $state = 'before_structure';
+                                        $startLine = $token[2];
+                                        $structType = 'trait';
                                     }
                                     break;
                             }
@@ -190,6 +201,13 @@ class ReflectionClosure extends ReflectionFunction
                             $state = $token == ',' ? 'use' : 'start';
                         }
                         break;
+                    case 'before_structure':
+                        if($is_array && $token[0] == T_STRING)
+                        {
+                            $structName = $token[1];
+                            $state = 'structure';
+                        }
+                        break;
                     case 'structure':
                         if(!$is_array)
                         {
@@ -197,22 +215,56 @@ class ReflectionClosure extends ReflectionFunction
                             {
                                 $open++;
                             }
-                            elseif($token === '{')
+                            elseif($token === '}')
                             {
                                 if(--$open == 0)
                                 {
+                                    $structures[] = array(
+                                        'type' => $structType,
+                                        'name' => $structName,
+                                        'start' => $startLine,
+                                        'end' => $endLine,
+                                    );
+                                    
                                     $state = 'start';
                                 }
                             }
+                        }
+                        else
+                        {
+                            $endLine = $token[2];
                         }
                         break;
                 }
             }
             
             static::$classes[$key] = $classes;
+            static::$structures[$key] = $structures;
         }
         
         return static::$classes[$key];
+    }
+    
+    protected function &getStructures()
+    {
+        $key = $this->getHashedFileName();
+        
+        if(!isset(static::$structures[$key]))
+        {
+            $this->getClasses();
+        }
+        
+        return static::$structures[$key];
+    }
+    
+    public function isStatic()
+    {
+        if($this->isStaticClosure === null)
+        {
+            $this->isStaticClosure = strtolower(substr($this->getCode(), 0, 6)) === 'static';
+        }
+        
+        return $this->isStaticClosure;
     }
     
     public function getCode()
@@ -246,9 +298,14 @@ class ReflectionClosure extends ReflectionFunction
             $_file = var_export($fileName, true);
             $_dir = var_export(dirname($fileName), true);
             $_namespace = var_export($ns, true);
-            $_class = var_export($className, true);
+            $_class = var_export(trim($className, '\\'), true);
+            $_function = $ns . ($ns == '' ? '' : '\\') . '{closure}';
+            $_method = ($className == '' ? '' : trim($className, '\\') . '::') . $_function;
+            $_function = var_export($_function, true);
+            $_method = var_export($_method, true);
+            $_trait = null;
             
-            
+            $hasTraitSupport = defined('T_TRAIT_C');
             $tokens = &$this->getTokens();
             $state = 'start';
             $open = 0;
@@ -266,10 +323,26 @@ class ReflectionClosure extends ReflectionFunction
                 switch($state)
                 {
                     case 'start':
-                        if($is_array && $token[0] === T_FUNCTION)
+                        if($is_array && ($token[0] === T_FUNCTION || $token[0] === T_STATIC))
                         {
                             $code .= $token[1];
-                            $state = 'function';
+                            $state = $token[0] === T_FUNCTION ? 'function' : 'static';
+                        }
+                        break;
+                    case 'static':
+                        if($is_array && ($token[0] === T_WHITESPACE || $token[0] === T_COMMENT || $token[0] === T_FUNCTION))
+                        {
+                            $code .= $token[1];
+                           
+                            if($token[0] === T_FUNCTION)
+                            {
+                                $state = 'function';
+                            }
+                        }
+                        else
+                        {
+                            $code = '';
+                            $state = 'start';
                         }
                         break;
                     case 'function':
@@ -330,6 +403,12 @@ class ReflectionClosure extends ReflectionFunction
                                 case T_CLASS_C:
                                     $token[1] = $_class;
                                     break;
+                                case T_FUNC_C:
+                                    $token[1] = $_function;
+                                    break;
+                                case T_METHOD_C:
+                                    $token[1] = $_method;
+                                    break;
                                 case T_COMMENT:
                                     if(substr($token[1], 0, 8) === '#trackme')
                                     {
@@ -356,6 +435,33 @@ class ReflectionClosure extends ReflectionFunction
                                     $new_key_word = true;
                                     $state = 'new';
                                     break 2;
+                                default:
+                                    if($hasTraitSupport && $token[0] == T_TRAIT_C)
+                                    {
+                                        if($_trait === null)
+                                        {
+                                            $startLine = $this->getStartLine();
+                                            $endLine = $this->getEndLine();
+                                            $structures = &$this->getStructures();
+                                            
+                                            $_trait = '';
+                                            
+                                            foreach($structures as &$struct)
+                                            {
+                                                if($struct['type'] === 'trait' &&
+                                                   $struct['start'] <= $startLine &&
+                                                   $struct['end'] >= $endLine)
+                                                {
+                                                    $_trait = ($ns == '' ? '' : $ns . '\\') . $struct['name'];
+                                                    break;
+                                                }
+                                            }
+                                            
+                                            $_trait = var_export($_trait, true);
+                                        }
+                                        
+                                        $token[1] = $_trait;
+                                    }
                             }
                             
                             $code .= $token[1];
