f453f32784cb29f626f2de9d04204fc9049f55d5
Deleu <deleugyn@gmail.com>
2020-07-20 19:00:47 +0200

    Add more cases to assertion

--- tests/ReflectionClosure6Test.php
+++ tests/ReflectionClosure6Test.php
@@ -15,10 +15,18 @@ final class ReflectionClosure6Test extends \PHPUnit\Framework\TestCase
 
     public function testUnionTypes()
     {
-        $f1 = fn() : string|int => 1;
-        $e1 = 'fn() : string|int => 1';
+        $f1 = fn() : string|int|false|Bar|null => 1;
+        $e1 = 'fn() : string|int|false|Bar|null => 1';
+
+        $f2 = fn() : Foo|Bar => 1;
+        $e2 = 'fn() : Foo|Bar => 1';
+
+        $f3 = fn() : false => false;
+        $e3 = 'fn() : false => false';
 
         $this->assertEquals($e1, $this->c($f1));
+        $this->assertEquals($e2, $this->c($f2));
+        $this->assertEquals($e3, $this->c($f3));
     }
 
     public function testMixedType()
