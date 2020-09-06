2274a2c8389b4d906bd0c7d4f9a3019527131f37
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-09-06 22:47:12 +0100

    Code style

--- tests/NamespaceFullyQualifiedTest.php
+++ tests/NamespaceFullyQualifiedTest.php
@@ -9,14 +9,15 @@ final class NamespaceGroupTest extends \PHPUnit\Framework\TestCase
 {
     public function test_namespace_fully_qualified()
     {
-        $f = function (){ new \A; };
-        $e = 'function (){ new \A; }';
+        $f = function () { new \A; };
+        $e = 'function () { new \A; }';
         $this->assertEquals($e, $this->c($f));
     }
 
     protected function c(Closure $closure)
     {
         $r = new ReflectionClosure($closure);
+
         return $r->getCode();
     }
 }
