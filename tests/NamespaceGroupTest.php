522c9b64b1cacc5c4ae334aa7470347b85029e2e
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-09-06 22:47:32 +0100

    Code style

--- tests/NamespaceGroupTest.php
+++ tests/NamespaceGroupTest.php
@@ -13,14 +13,15 @@ final class NamespaceFullyQualifiedTest extends \PHPUnit\Framework\TestCase
 {
     public function test_namespace_fully_qualified()
     {
-        $f = fn():Forest => new Forest;
-        $e = 'fn():\Foo\Baz\Qux\Forest => new \Foo\Baz\Qux\Forest';
+        $f = fn(): Forest => new Forest;
+        $e = 'fn(): \Foo\Baz\Qux\Forest => new \Foo\Baz\Qux\Forest';
         $this->assertEquals($e, $this->c($f));
     }
 
     protected function c(Closure $closure)
     {
         $r = new ReflectionClosure($closure);
+
         return $r->getCode();
     }
 }
